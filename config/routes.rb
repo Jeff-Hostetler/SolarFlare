b3e5bd12e12cdf17f66d931b2addaefe4bd8c89a
Jeff Hostetler <hostetjm@gmail.com>
2014-10-17 14:55:52 -0600

    added about us and products page without content

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root "pages#index"
+get "about" => "pages#about", name: :about
+get "products" => "pages#products", name: :products
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
