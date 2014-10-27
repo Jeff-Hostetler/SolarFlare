62511b9981d6389e73f94d4d5238c77b1f2e91f5
Jeff Hostetler <hostetjm@gmail.com>
2014-10-27 16:47:07 -0600

    added create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
 
   def index
-    
+
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      respond_to do |format|
+        if @user.save
+          format.html { redirect_to users_url, notice: 'User was successfully created.' }
+        end
+      end
+  end
+
+  def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :project_name,
+      :join_mailing_list)
+  end
+
 
 end
