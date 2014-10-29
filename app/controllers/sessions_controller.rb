6ea0d05c52c15afad199ac20a652466986e9c74b
Jeff Hostetler <hostetjm@gmail.com>
2014-10-29 16:44:21 -0600

    can now login user, navigation not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,11 +13,9 @@ class SessionsController < ApplicationController
       end
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
-  
   def destroy
+    User.find_by(params[:id]).destroy
+    redirect_to login_path
   end
 
 end
