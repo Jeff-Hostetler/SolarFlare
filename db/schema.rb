bf2da8664dff4a5ae4d535983d4cb1f8140dbe81
Jeff Hostetler <hostetjm@gmail.com>
2014-10-31 15:09:43 -0600

    added phone number to users for text alerts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031184238) do
+ActiveRecord::Schema.define(version: 20141031210822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20141031184238) do
     t.string  "address_city"
     t.string  "address_state"
     t.string  "address_zip"
+    t.string  "phone_number"
   end
 
 end
