8b8784ddf293e58ba02b2a17c531ed2c288ae3be
Jeff Hostetler <hostetjm@gmail.com>
2014-10-31 15:14:04 -0600

    added textalert and emailalert to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031210822) do
+ActiveRecord::Schema.define(version: 20141031211135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20141031210822) do
     t.string  "address_state"
     t.string  "address_zip"
     t.string  "phone_number"
+    t.boolean "text_alert"
+    t.boolean "email_alert"
   end
 
 end
