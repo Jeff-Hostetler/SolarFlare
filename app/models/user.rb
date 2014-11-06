93030caceac9b4fe17c100eb2f78b2262d935e2c
Jeff Hostetler <hostetjm@gmail.com>
2014-11-06 14:28:29 -0700

    finished writing a test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   before_save {|user| user.email=user.email.downcase}
 
   has_secure_password
-  validates_presence_of :password, :on => :create
+  # validates_presence_of :password, :on => :create
+  validates_presence_of :password_confirmation, :on => :create
   validates_presence_of :email, presence: true, uniqueness: true
   validates_presence_of :first_name, presence: true
   validates_presence_of :last_name, presence: true
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -22,6 +22,32 @@ feature "Users" do
     click_on "Sign Up"
     expect(page).to have_content("Hello, Test Userguy")
 
+  end
+
+  scenario "User tries to sign up with invalid info" do
+
+    visit root_path
+    click_on "Optimize Your System"
+    click_on "Create User Profile"
+    click_on "Sign Up"
+    expect(page).to have_content("5 errors prohibited this user from being saved:")
+
+    fill_in "First name", with: "Test"
+    click_on "Sign Up"
+    expect(page).to have_content("4 errors prohibited this user from being saved:")
+
+    fill_in "Last name", with: "Test"
+    click_on "Sign Up"
+    expect(page).to have_content("3 errors prohibited this user from being saved:")
+
+    fill_in "Email", with: "test@test.com"
+    click_on "Sign Up"
+    expect(page).to have_content("2 errors prohibited this user from being saved:")
+
+    fill_in "Password", with: "Test"
+    fill_in "Password confirmation", with: "test"
+    click_on "Sign Up"
+    expect(page).to have_content("1 error prohibited this user from being saved:")
 
   end
 
