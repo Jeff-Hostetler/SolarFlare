a54e6ce4da71e313d1d2354b9cb33858167b08c5
Jeff Hostetler <hostetjm@gmail.com>
2014-11-08 21:00:14 -0700

    added password length validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
   # validates_presence_of :password, :on => :create
-  validates_presence_of :password_confirmation, :on => :create
+  validates :password, length: {minimum: 8, maximum: 20}
+  validates_presence_of :password_confirmation, :on => :create, length: {minimum: 8, maximum: 20}
   validates_presence_of :email, presence: true, uniqueness: true
   validates_presence_of :first_name, presence: true
   validates_presence_of :last_name, presence: true
