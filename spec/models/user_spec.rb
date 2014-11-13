c8e1a415ce253e414496c3225437b8712ba339a0
Jeff Hostetler <hostetjm@gmail.com>
2014-11-12 18:38:13 -0700

    increased test coveratge on user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,33 @@ require 'rails_helper'
 
 describe User do
 
+  it "requires first, last, email, password/conf" do
+    user = User.new
+    user.valid?
+
+    expect(user.errors[:first_name].present?).to eq(true)
+    expect(user.errors[:last_name].present?).to eq(true)
+    expect(user.errors[:email].present?).to eq(true)
+    expect(user.errors[:password].present?).to eq(true)
+    expect(user.errors[:password_confirmation].present?).to eq(true)
+
+
+    user = User.new(
+      first_name: "Test",
+      last_name: "Test",
+      email: "test@test.com",
+      password: "123456",
+      password_confirmation: "123456")
+
+      user.valid?
+
+      expect(user.errors[:first_name].present?).to eq(false)
+      expect(user.errors[:last_name].present?).to eq(false)
+      expect(user.errors[:email].present?).to eq(false)
+      expect(user.errors[:password].present?).to eq(false)
+      expect(user.errors[:password_confirmation].present?).to eq(false)
+  end
+
   it "has a unique email" do
     User.create!(
       first_name: "Test",
@@ -13,34 +40,42 @@ describe User do
 
     user = User.new(email: "test@test.com")
     user.valid?
+
     expect(user.errors[:email].present?).to eq(true)
 
 
     user2 = User.new(email: "test2@test.com")
     user2.valid?
+
     expect(user2.errors[:email].present?).to eq(false)
   end
 
   it "has a password between 6 and 20 characters" do
+
     user = User.new(
       email: "test@test.com",
       password: "1234",
       password_confirmation: "1234")
     user.valid?
+
     expect(user.errors[:password].present?).to eq(true)
+
 
     user = User.new(
       email:"test@test.com",
       password: "123456",
       password_confirmation: "123456")
     user.valid?
+
     expect(user.errors[:password].present?).to eq(false)
+
 
     user = User.new(
       email: "test@test.com",
       password: "123456789012345678901",
       password_confirmation: "123456789012345678901")
     user.valid?
+
     expect(user.errors[:password].present?).to eq(true)
   end
 
