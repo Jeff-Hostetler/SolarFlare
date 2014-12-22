9f1c382c6fe2d0491639fd1fcc6183efe5844cec
Jeff Hostetler <hostetjm@gmail.com>
2014-12-22 16:51:32 -0700

    set up Mail defaults for sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,16 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@exmaple.com'
   layout 'mailer'
+
+  Mail.defaults do
+    delivery_method :smtp, {
+      :address => 'smtp.sendgrid.net',
+      :port => '25',
+      :domain => 'heroku.com',
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :authentication => :plain,
+      :enable_starttls_auto => true
+    }
+  end
 end
