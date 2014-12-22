df894643b66a4a22daa0b0f8a3a4138bc09ef9c8
Jeff Hostetler <hostetjm@gmail.com>
2014-12-22 11:58:40 -0700

    added create sensor to attempt with postman

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -5,8 +5,15 @@ class SensorsController<ApplicationController
   end
   before_action :confirm_current_user
 
+  skip_before_action :verify_authenticity_token
+
   def current_reading
     @sensor = @user.sensors.all
+  end
+
+  def create
+    @sensor = @user.sensor.new
+    @sensor.save
   end
 
   private
