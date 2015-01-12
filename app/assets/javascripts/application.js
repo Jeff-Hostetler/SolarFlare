785ecf2667aa93ae63c2edf1c026274fd1aa6ccb
Jeff Hostetler <hostetjm@gmail.com>
2015-01-12 14:11:47 -0700

    added epoch graph for real time readings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,39 @@
 //= require_tree .
 //= require bootstrap
 
-setInterval(function() {
-  console.log('in interval')
-  $.getJSON(window.location.origin + '/users/'+ $("#userInfo").val() +'/current_reading', function(data) {
-     $('body').append(data.data_point);
-  })
-}, 1000);
+var data_time = 1370044800;
+$(function(){
+  var chartData = [{
+    label: 'Layer 1',
+    values: [
+      {time: 1370044801, y: 1000 },
+      {time: 1370044901, y: 4000 }
+    ]
+  }];
+
+  var data_bits = [{
+    layer: 'Layer 1',
+    values: [{time: Date.now()/1000, y: 0}, {time: Date.now()/1000, y: 1000}]
+  }];
+
+  var chart = $('#myChart').epoch({
+    type: 'time.line',
+    data: data_bits,
+    axes: ['bottom', 'right', 'left']
+  });
+
+  setInterval(function() {
+    $.getJSON(window.location.origin + '/users/'+ $("#userInfo").val() +'/current_reading', function(responseData) {
+
+      var newDataBit = [{
+        time: (Date.now()/1000),
+        y: responseData.data_point
+      }];
+      console.log(newDataBit);
+      console.log(data_bits);
+
+      chart.push(newDataBit);
+
+    });
+  }, 1000);
+});
