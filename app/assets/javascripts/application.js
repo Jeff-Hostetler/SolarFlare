fc776634922aab5e4948b01b4198ac2f12e25206
Jeff Hostetler <hostetjm@gmail.com>
2015-01-13 14:09:56 -0700

    added some jquery to dim the real time graph when reading is low

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,14 +31,21 @@ $(function(){
 
   setInterval(function() {
     $.getJSON(window.location.origin + '/users/'+ $("#userInfo").val() +'/current_reading', function(responseData) {
-
+      /*push new data into chart with ajax call */
       var newData = [{
         time: (Date.now()/1000),
         y: responseData.data_point
       }];
-
       chart.push(newData);
-
+      /*adjust color of background based on the light level reading*/
+      if (responseData.data_point < 400){
+        $('#realTimeChart').addClass('dark');
+        $('#realTimeChart').removeClass('bright');
+      }
+      else{
+        $('#realTimeChart').addClass('bright');
+        $('#realTimeChart').removeClass('dark');
+      }
     });
   }, 1000);
 });
