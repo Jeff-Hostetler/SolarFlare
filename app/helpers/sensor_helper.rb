c16a600395edee8a23aed37280492494457e06b9
Jeff Hostetler <hostetjm@gmail.com>
2015-01-26 16:53:05 -0700

    keen ajax call

--- app/helpers/sensor_helper.rb
+++ app/helpers/sensor_helper.rb
@@ -27,17 +27,34 @@ module SensorHelper
     data
   end
   #have to test after getting some data over a couple of days
+
   def keen_weekly_sensor_data
-    Keen.average("sensors", :target_property => "data_point",
-                            :percentile => 90,
-                            :timeframe => {
-                              start: "#{Time.now - 3.day}",
-                              end: "#{Time.now}"
-                            },
-                            :filters => [{
-                              "property_name" => "user_id",
-                              "operator" => "eq",
-                              "property_value" => "#{@user.id}"
-    }])
+    data = []
+    i = 0
+    until i > 7
+      data_point = (Keen.average("sensors", :target_property => "data_point",
+      :percentile => 90,
+      :timeframe => {
+        start: "#{Time.now - (i+1).day}",
+        end: "#{Time.now - (i).day}"
+    },
+    :filters => [{
+      "property_name" => "user_id",
+      "operator" => "eq",
+      "property_value" => "#{@user.id}"
+      }])).to_f
+
+
+      if data_point == nil
+        data_percentage_point = 0
+      else
+        data_percentage_point = data_point/1000
+      end
+
+      data<< [(Time.current-i.day).to_date, data_percentage_point]
+      i += 1
+    end
+
+    data
   end
 end
